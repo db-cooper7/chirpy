1d84a84066fda5ecc7c51c501be543a28ebe41f2
db-cooper7 <fpgr07@outlook.com>
2025-06-16 17:03:25 +0100

    add app and readiness handlers

--- main.go
+++ main.go
@@ -5,14 +5,19 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	const port = "8080"
 	const rootFilePath = "."
-	const logoFilePath = "./assets/logo.png"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(rootFilePath)))
-	mux.Handle("/assets", http.FileServer(http.Dir(logoFilePath)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(rootFilePath))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Handler: mux,
